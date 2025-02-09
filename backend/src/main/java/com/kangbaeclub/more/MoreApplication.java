8a77611228e3244e0afdee9d96218f6421fe3081
joonamin <joonamin44@gmail.com>
2025-02-09 21:23:36 +0900

    chore: linter 및 gradle task를 통한 git hook 관리

--- backend/src/main/java/com/kangbaeclub/more/MoreApplication.java
+++ backend/src/main/java/com/kangbaeclub/more/MoreApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoreApplication.class, args);
+    }
 }
