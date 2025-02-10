1d5fe9cd9997142b1fdce7a53edb26f47229e442
Kang Minjun <97105309+joonamin@users.noreply.github.com>
2025-02-10 23:56:19 +0900

    chore: linter 및 gradle task를 통한 git hook 관리 (#6)
    * chore: linter 및 gradle task를 통한 git hook 관리
    
    * fix: set credential job
    
    * chore: fix pre-commit hook
    
    * add shebang to the script

--- backend/src/test/java/com/kangbaeclub/more/MoreApplicationTests.java
+++ backend/src/test/java/com/kangbaeclub/more/MoreApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
